6846f337c21527762727512585348d5e7111720d
greyTurtleDove <radek29c@gmail.com>
2026-05-02 19:55:39 +0200

    feat/frontend/api-url

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,7 +11,7 @@ export default function LobbyPage() {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch("http://localhost:8000/login", {
+            const response = await fetch("http://localhost:3001/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -33,7 +33,7 @@ export default function LobbyPage() {
 
     const handleRegister = async () => {
         try {
-            const response = await fetch("http://localhost:8000/register", {
+            const response = await fetch("http://localhost:3001/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
