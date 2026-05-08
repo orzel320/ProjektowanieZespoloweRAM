81ecf7be3b0be84539c1ea1425a25a87b4dee49a
Albert Piekarski <31486978+abi5001@users.noreply.github.com>
2026-05-08 18:07:11 +0200

    feat: lobby rooms backend
    not really tested

--- backend/src/lobby/lobby.module.ts
+++ backend/src/lobby/lobby.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LobbyGateway } from './lobby.gateway';
 import { LobbyService } from './lobby.service';
+import { LobbyController } from './lobby.controller';
 
 @Module({
+  controller: [LobbyController],
   providers: [LobbyGateway, LobbyService],
   exports: [LobbyService],
 })
